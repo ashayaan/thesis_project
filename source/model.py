b3b2a62672ac8c3c0e8334610d1221ce5c7efaca
ashayaan <a.shayaan@gmail.com>
2019-04-10 10:13:04 +0530

    Initial commit

---
+++ source/model.py
@@ -0,0 +1,16 @@
+import torch
+import torch.nn as nn
+import torch.optim as optim
+import numpy as np
+import pandas as pd
+
+class Network(nn.Module):
+	def __init__(self):
+		
+
+
+
+
+'''Testing network'''
+if __name__ == '__main__':
+	
